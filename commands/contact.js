31e993b6f7b8ac3ec6e0feb962e1c62ab6d86c3d
bigmanjtech <bigmanj429@gmail.com>
2026-08-06 00:34:40 +0300

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -3,7 +3,7 @@ module.exports = {
     execute: async (ctx, { config }) => {
         const contact =
             `📞 *Contact Us*\n\n` +
-            `» *Telegram:* t.me/bigmanj9\n` +
+            `» *Telegram:* t.me/bigmanj09\n` +
             `» *WhatsApp:* wa.me/255636756591\n` +
             `» *Email:* bigmanj.tech@gmail.com\n\n` +
             `Our support team is available 24/7.`;
